5bca72c352c741526e000de838174ff25a66a54b
Shibashis Deb <shibashisdeb.ee@gmail.com>
2025-10-06 00:43:32 +0530

    drop faulty router

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+          <Index />
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
